9482e9fb9b5975066306b8724699ef2356395834
OzzkanBulut <ozkanb66@gmail.com>
2024-06-03 07:29:19 +0300

    Increasing subsequence

--- 01_Sliding_Window_Pattern/src/Main.java
+++ 01_Sliding_Window_Pattern/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {10,9,2,5,3,7,101,18}; // 2 3 7 101
+        System.out.println(longestIncreasing(arr));
+    }
+
+    public static int longestIncreasing(int[] arr){
+        int length = 0,maxLength =0 ;
+        int min = Integer.MAX_VALUE;
+
+        for(Integer num:arr){
+            min = Integer.min(num,min);
+        }
+        // {10,9,2,5,3,7,101,18};  2 3 7 101
+        for(int i=0;i<arr.length;i++){
+
+
+
+
+
+        }
+
+
+
+        return 0;
+
     }
 }
