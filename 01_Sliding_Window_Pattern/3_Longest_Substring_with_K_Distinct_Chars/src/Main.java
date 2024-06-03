8f8a8d0070fd05113a15adc18a4735470f3d07dd
OzzkanBulut <ozkanb66@gmail.com>
2024-06-03 05:34:44 +0300

    Longest substring with k distinct char again

--- 01_Sliding_Window_Pattern/3_Longest_Substring_with_K_Distinct_Chars/src/Main.java
+++ 01_Sliding_Window_Pattern/3_Longest_Substring_with_K_Distinct_Chars/src/Main.java
@@ -3,35 +3,35 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(findLongestSubString("arci",2));
+        System.out.println(findLongestSubString("araaci", 2));
 
 
     }
 
-    public static int findLongestSubString(String s, int k){
-
-        if(s==null || s.isEmpty() || s.length()<k){
-            throw new ArithmeticException("error");
+    public static int findLongestSubString(String s, int k) {
+        if (s == null || s.isEmpty() || s.length() < k) {
+            return 91;
         }
-        int windowStart = 0;
-        int maxLength =0;
+        int windowStart = 0, maxLength = 0;
+        Map<Character, Integer> frequencyMap = new HashMap<>();
 
-        Map<Character,Integer> frequencyMap = new HashMap<>();
-
-        for(int windowEnd=0;windowEnd<s.length();windowEnd++){
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
             char rightChar = s.charAt(windowEnd);
-            frequencyMap.put(rightChar,frequencyMap.getOrDefault(rightChar,0)+1);
-            while(frequencyMap.size()>k){
-                char leftChar = s.charAt(windowStart);
-                frequencyMap.put(leftChar,frequencyMap.get(leftChar)-1);
-                if(frequencyMap.get(leftChar)==0){
+            frequencyMap.put(rightChar, frequencyMap.getOrDefault(rightChar, 0) + 1);
+            while (frequencyMap.size() > k) {
+                char leftChar = s.charAt(windowEnd);
+                frequencyMap.put(leftChar, frequencyMap.get(leftChar) - 1);
+                if (frequencyMap.get(leftChar) == 0) {
                     frequencyMap.remove(leftChar);
                 }
                 windowStart++;
             }
-            maxLength = Integer.max(maxLength,windowEnd-windowStart+1);
+            maxLength = Integer.max(maxLength, windowEnd - windowStart + 1);
         }
+
         return maxLength;
 
     }
+
+
 }
