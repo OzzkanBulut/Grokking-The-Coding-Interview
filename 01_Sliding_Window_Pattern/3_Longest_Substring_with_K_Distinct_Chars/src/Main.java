4fe4ebf602440c28d8aaa095f1a9de8d26304eb3
OzzkanBulut <ozkanb66@gmail.com>
2024-06-03 08:46:37 +0300

    longest sub without repeating

--- 01_Sliding_Window_Pattern/3_Longest_Substring_with_K_Distinct_Chars/src/Main.java
+++ 01_Sliding_Window_Pattern/3_Longest_Substring_with_K_Distinct_Chars/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(findLongestSubString("araaci", 2));
+        System.out.println(findLongestSubString("araaaci", 2));
 
 
     }
